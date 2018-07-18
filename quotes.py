c73fb1186c922f71a4ef27f4b232d97d314f01dc
Nic Mews-Schmuck <nems608@gmail.com>
2018-07-18 16:56:29 -0500

    Add error and rate-limiting handling to quotes API

--- quotes.py
+++ quotes.py
@@ -3,6 +3,8 @@ Looks up minute-specific quotes for stock tickers
 """
 import requests
 import json
+import time
+import datetime
 apikey = None
 
 def load_apikey():
@@ -23,11 +25,26 @@ def get_quote_data(ticker):
               'apikey': apikey}
     resp = requests.get('https://www.alphavantage.co/query', params=params)
     resp.raise_for_status()
-    return json.loads(resp.text)
+    quote_data = json.loads(resp.text)
+    if 'Error Message' in quote_data:
+        if 'Invalid API call' in quote_data['Error Message']:
+            return get_quote_data(ticker)
+        else:
+            raise Exception('Quote Error: {}, {}'.format(ticker, quote_data['Error Message']))
+    elif 'Information' in quote_data:
+        print('Call limit reached. Sleeping...')
+        m = datetime.datetime.now().minute
+        n = datetime.datetime.now().minute
+        while n == m:
+            time.sleep(1)
+            n = datetime.datetime.now().minute
+        return get_quote_data(ticker)
+    return quote_data
 
-def get_price_data(ticker):
+def get_price_volume_data(ticker):
     quote_data = get_quote_data(ticker)
     quote_data = quote_data['Time Series (1min)']
-    price_data = {}
+    price_volume_data = {}
     for time,data in quote_data.items():
-        price_data[time] = data['1. open']
+        price_volume_data[time] = {'price':data['1. open'], 'volume':data['5. volume']}
+    return price_volume_data
